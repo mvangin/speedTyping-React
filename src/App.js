9864a29f46ad76b17c90c4f24dcf922c1a68d3de
Mgin <mavangin@gmail.com>
2020-08-06 16:04:52 -0700

    fix wpm bug

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React, { useState, useEffect, useRef } from "react"
+import React from "react"
 import GameLogic from "./hooks/GameLogic"
 import TypeCorrect from "./TypeCorrect"
 
 
 function App() {
   const {
-      handleInput,
-      handleTimerSubmit,
-      StartNewGame,
-      handleInputTimer,
-      randomText,
-      userText,
-      timer,
-      textDisable,
-      newGameDisable,
-      timerInputDisable,
-      inputTimer,
-      textArea,
-      wpm,
-      correctedWpm,
+    handleInput,
+    handleTimerSubmit,
+    StartNewGame,
+    handleInputTimer,
+    randomText,
+    userText,
+    timer,
+    textDisable,
+    newGameDisable,
+    timerInputDisable,
+    inputTimer,
+    textArea,
+    wpm,
+    correctedWpm,
   } = GameLogic();
 
   return (
@@ -27,30 +27,49 @@ function App() {
         Speed Typing Test
         </h1>
 
-      <div id="randomText">
-        <TypeCorrect randomText={randomText} userText={userText}  />
+
+
+      <div id="contentWrapper">
+
+        <div id="textWrapper">
+        <div id="randomText">
+            <TypeCorrect randomText={randomText} userText={userText} />
+          </div>
+          <textarea ref={textArea} disabled={textDisable} value={userText} onChange={handleInput} placeholder="Start typing to begin" />
+
+        </div>
+
+        <div id="wpm">
+
+          <h3 id="wpmText">
+            {`Words Per Minute: ${wpm}`}
+          </h3>
+
+          <h3>
+            {`Corrected Words Per Minute: \n ${correctedWpm}`}
+          </h3>
+
+        </div>
+
       </div>
 
-
-      <textarea ref={textArea} disabled={textDisable} value={userText} onChange={handleInput} placeholder="Start typing to begin" />
       <div id="inputWrapper">
-        Number of seconds: <input type="number" min="1" disabled={timerInputDisable} value={inputTimer} onChange={handleInputTimer} placeholder="10" />
+        Number of seconds: <input type="number" min="1" disabled={timerInputDisable} value={inputTimer} onChange={handleInputTimer}/>
         <button onClick={handleTimerSubmit} disabled={timerInputDisable} > submit </button>
       </div>
 
-      <h2>
+      <h2 id="timer">
         {`Timer: ${timer} seconds`}
       </h2>
 
+
+
+
       <button id="StartNewGame" disabled={newGameDisable} onClick={StartNewGame}> Start New Game </button>
 
-      <h3>
-        {`Words Per Minute: ${wpm}`}
-      </h3>
 
-      <h3>
-        {`Corrected Words Per Minute: ${correctedWpm}`}
-      </h3>
+
+
 
     </>
   )
--- src/hooks/GameLogic.js
+++ src/hooks/GameLogic.js
@@ -7,17 +7,40 @@ function GameLogic() {
     const [textDisable, setTextDisable] = useState(false)
     const [newGameDisable, setNewGameDisable] = useState(false)
     const [timerInputDisable, setTimerInputDisable] = useState(false)
-    const [inputTimer, setInputTimer] = useState("")
+    const [inputTimer, setInputTimer] = useState("10")
     const textArea = useRef(null)
     const [wpm, setWpm] = useState("")
     const [correctedWpm, setCorrectedWpm] = useState("")
     const [endGame, setEndGame] = useState(false)
 
-    let defaultText = 
-    "Dave wasn't exactly sure how he had ended up in this predicament. He ran through all the events that had lead to this current situation and it still didn't make sense. He wanted to spend some time to try and make sense of it all, but he had higher priorities at the moment. The first was how to get out of his current situation of being in a tree with snow falling all around and no way for him to get down. "
+    let defaultText =
+        "Dave wasn't exactly sure how he had ended up in this predicament. He ran through all the events that had lead to this current situation and it still didn't make sense. He wanted to spend some time to try and make sense of it all, but he had higher priorities at the moment. The first was how to get out of his current situation of being in a tree with snow falling all around and no way for him to get down. "
 
     const [randomText, setRandomText] = useState(defaultText)
 
+
+    function calculateWpm() {
+        let correctNumTally = 0;
+
+        let filteredRandomText = randomText.trim().split(" ").filter(word => word !== "")
+        let wordArray = userText.trim().split(" ")
+
+        let filteredWords = wordArray.filter(word => word !== "")
+
+        filteredWords.forEach((item, index) => {
+            if (index < filteredRandomText.length) {
+                if (item === filteredRandomText[index]) {
+                    correctNumTally += 1;
+                }
+            } 
+        })
+
+        let wpm = Math.round(filteredWords.length / (inputTimer / 60))
+        let wpmCorrected = Math.round((correctNumTally / (inputTimer / 60)))
+        console.log(wpm)
+        return ({ wpm: wpm, wpmCorrected: wpmCorrected })
+
+    }
 
     function handleInput(e) {
         if (userText === "" && initialize === false) {
@@ -27,6 +50,13 @@ function GameLogic() {
             setWpm("")
         }
         setUserText(e.target.value);
+        console.log(e.target.value)
+        if (e.target.value[e.target.value.length-1] === " ") {
+            let { wpm } = calculateWpm();
+            setWpm(wpm)
+        }
+
+
     }
 
 
@@ -60,7 +90,6 @@ function GameLogic() {
     useEffect(() => {
         if (endGame) {
             setDefaultValues();
-            calculateWpm();
 
             function setDefaultValues() {
                 setTextDisable(true)
@@ -68,25 +97,10 @@ function GameLogic() {
                 setEndGame(false)
             }
 
-            function calculateWpm() {
-                let correctNumTally = 0;
+            let {wpm, wpmCorrected } = calculateWpm();
+            setCorrectedWpm(wpmCorrected)
+            setWpm(wpm)
 
-                let filteredRandomText = randomText.trim().split(" ").filter(word => word !== "")
-                let wordArray = userText.trim().split(" ")
-
-                let filteredWords = wordArray.filter(word => word !== "")
-
-                filteredWords.forEach((item, index) => {
-                    if (index < filteredRandomText.length - 1) {
-                        if (item === filteredRandomText[index]) {
-                            correctNumTally += 1;
-                        }
-                    }
-                })
-
-                setWpm(filteredWords.length / (inputTimer / 60))
-                setCorrectedWpm(correctNumTally / (inputTimer / 60))
-            }
         }
 
     }, [endGame])
