bc6299d2e9ce4c9fae689923d763867f79821d6a
Mgin <mavangin@gmail.com>
2020-08-06 20:51:39 -0700

    Updates

---
+++ precache-manifest.a05806327129c00782c3cc0f07cd4a6a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "211a6896e15139e54fca54121e294b5a",
+    "url": "/speedTyping-React/index.html"
+  },
+  {
+    "revision": "403bd7eb65b3d2cd7686",
+    "url": "/speedTyping-React/static/js/2.ebd63b2b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/speedTyping-React/static/js/2.ebd63b2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e962c5e91d66c5d828e",
+    "url": "/speedTyping-React/static/js/main.6074a814.chunk.js"
+  },
+  {
+    "revision": "dc1e69ba1d679c0d1e3d",
+    "url": "/speedTyping-React/static/js/runtime-main.b4e6998e.js"
+  }
+]);
